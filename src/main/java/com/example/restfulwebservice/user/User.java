516838236c85412139b44d3e629258b1845ab51b
hoon <1bun0403@gmail.com>
2021-06-05 11:00:00 +0900

    webhook test

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -21,7 +21,7 @@ import java.util.Date;
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 public class User {
     private Integer id;
-
+    // test
     @Size(min=2, message="Name은 2글자 이상 입력해 주세요.")
     @ApiModelProperty(notes = "사용자 이름을 입력해 주세요.")
     private String name;
